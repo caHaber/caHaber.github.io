90d224cb7509ff76923963804bbb4e6c66f14d9c
Casey Haber <cahaber@dons.usfca.edu>
2016-06-17 16:03:37 -0700

    Demo Finishing Up
    First full prototype is up and running as BubbleTest

--- UNDemo's/accessor.py
+++ UNDemo's/accessor.py
@@ -15,15 +15,20 @@ countries = pd.DataFrame(r3)
 countries = countries.drop(countries.columns[[2,3,4,5,6,7,8,9,10]], axis=1)
 countries.columns = ['Continent','Reference Area']
 
-r4 = pd.read_csv('worldPop.csv')
+r4 = pd.read_csv('RuralPop.csv')
 pop = pd.DataFrame(r4)
-col_list = [4, 54]
-pop = pop[col_list]
+col_list = [0,2,4]
+pop = pop.drop(pop.columns[[3,4,6]], axis=1)
+pop.columns = ['Reference Area','Sub Group','Year','Population']
+
+pop = pop[(pop['Year'] == 2000)]
 print(pop)
 
 
 result2 = pd.merge(result, countries, on='Reference Area')
-result2.to_csv('result.csv')
+
+result3 = pd.merge(result2, pop, on='Reference Area')
+result3.to_csv('result.csv')
 ##writer = csv.writer(open('output.csv', 'w'))
 ##for row in r:
 ##    print(row)
