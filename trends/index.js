d6895a24e3ce1af5aaf56bdfbb3fa769b193c9fc
Casey Haber <cahaber@dons.usfca.edu>
2016-12-28 13:46:13 -0800

    Fixing static demo

--- trends/index.js
+++ trends/index.js
@@ -1,19 +1,10 @@
 var googleTrends = require('google-trends-api');
 
-var options = {
-    geo: 'country code or name',
-    date: 'yyyymm',
-    keywords: ['some', 'list', 'of', 'keywords'],
-    category: 'some category',
-    timePeriod: {
-        type: enumerated string 'hour', 'day', 'month', or 'year'
-        value: number
-    }
-}
 
-googleTrends.apiMethod(options)
+googleTrends.hotTrends('US')
 .then(function(results){
-    console.log("Here are your google trend results!", results);
+    console.log(results)
+    // console.log("Here are your google trend results!", results);
 })
 .catch(function(err){
     console.log("there was an error :(", err);
