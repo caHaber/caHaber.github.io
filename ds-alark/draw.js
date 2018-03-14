71f2051005bb999f34b31b295c72c7e39a934c5b
Casey Haber <cahaber@dons.usfca.edu>
2018-03-13 21:16:42 -0700

    Small changes in color/startline

--- ds-alark/draw.js
+++ ds-alark/draw.js
@@ -17,8 +17,8 @@ var pallete =
 //Random Colors
 //['#E6E6E6','#909090','#2A2A2A','#C1F6BE','#BEDAFF','#17B8FE','#40EDC8','#FF8235','#FF85B8'];
 //Color brewer set 9 qualitative sets
-//  ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9'];
- ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'];
+ ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9'];
+//  ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'];
 
 
 function setup() {
