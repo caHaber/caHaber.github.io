82d61e4b5744f67b1a1253bfcaea9dc95fe90fa7
Casey Haber <cahaber@dons.usfca.edu>
2018-04-12 15:52:54 -0700

    Updated to useless pen width

--- github-frontend-draw/draw.js
+++ github-frontend-draw/draw.js
@@ -3,23 +3,49 @@
 var xhr = new XMLHttpRequest();
 var selectionComplete = 0;
 var colorPicked;
+var strokePicked;
 
 // Line seg is {mouseX, mouseY, pmouseX, pmouseY}
 var path = {lineSegs: [], width: null, height: null, strokeColor: null, strokeWidth: 7};
 
 // For local development you must run the post server and the website
 var URL
-// ='http://localhost:3000';
-= 'http://50.1.86.208:3000/';
+='http://localhost:3000';
+// = 'http://50.1.86.208:3000/';
 
 
 var pallete =
 //Random Colors
 //['#E6E6E6','#909090','#2A2A2A','#C1F6BE','#BEDAFF','#17B8FE','#40EDC8','#FF8235','#FF85B8'];
 //Color brewer set 9 qualitative sets
- ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9'];
+//  ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9'];
 //  ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'];
-
+// HSL Pallette
+["lightgrey",
+"grey",
+"black",
+"red",
+"slategrey",
+"royalblue",
+"green",
+"Beige",
+"purple",
+"pink",
+"yellow",
+"lightblue"];
+//Lighter Version
+// ["#aabbea",
+// "#b6c489",
+// "#deb1e0",
+// "#aad9a7",
+// "#ecaaae",
+// "#55cdd8",
+// "#dfbc94",
+// "#7cccee",
+// "#e6e7af",
+// "#80e4d6",
+// "#a3daba",
+// "#9cd9d4"]
 
 function setup() {
    createCanvas(windowWidth, windowHeight); 
@@ -44,20 +70,21 @@ function draw(){
 
     ellipse(windowWidth/2,windowHeight/2,circleSize,circleSize);
 
-    if(circleSize > windowWidth *1.315 && circleSize > windowHeight*1.315){
+    if(circleSize > windowWidth*1.315 && circleSize > windowHeight*1.315){
         selectionComplete = 1;
-        fill("none");
-        clear();
-
-        circleSize = 0;
-        setPen("black",4);
-        drawStarterLine();
-        setPen(colorPicked,+path.strokeWidth);
+        drawStage();
         noLoop();
     }
-   
-    circleSize = circleSize + 20;
-    
+    circleSize = circleSize + 20;  
+}
+
+function drawStage() {
+    fill("none");
+    clear();
+    circleSize = 0;
+    setPen("black",4);
+    drawStarterLine();
+    setPen(colorPicked,+path.strokeWidth);
 }
 
 
@@ -65,26 +92,58 @@ function drawColorChoices(){
     var color = 0;
     strokeWeight(0);
     for(var i = 0; i < 3; i++){
-        for(var j = 0; j < 3; j++){
+        for(var j = 0; j < 4; j++){
             fill(pallete[color]);
-            rect(i * (windowWidth/3), j * (windowHeight/3), windowWidth/3, windowHeight/3);
+            rect(i * (windowWidth/3), j * (windowHeight/4), windowWidth/3, windowHeight/4);
             color++;
         }
     }
     strokeWeight(+path.strokeWidth);
 }
 
+function drawPenChoices(){
+    clear();
+    var color = 0;
+    var size = 5;
+    strokeWeight(0);
+    for(var j = 0; j < 3; j++){
+        for(var i = 0; i < 4; i++){
+            fill(colorPicked);
+            ellipse(j * (windowWidth/3) + (windowWidth/3)/2, i * (windowHeight/4) + windowHeight/8 , size,size);
+            color++;
+            size += 5;
+        }
+    }
+    strokeWeight(+path.strokeWidth);
+
+}
+
+var secondStage = 0;
 function touchStarted() {
-    if(!selectionComplete) {
+    if(!selectionComplete && secondStage === 0) {
         colorPicked = get(mouseX,mouseY);
         stroke(colorPicked[0],colorPicked[1],colorPicked[2],colorPicked[3]);
-       
+    //    selectionComplete =
         fill("white");
+        drawPenChoices();
+        secondStage = 1;
+        // loop();
+        return false;
+    } else if (secondStage === 1) {
+        console.log("drawStage");
+        strokePicked = getStroke((mouseX,mouseY));
+        // drawStage()
+        secondStage = 2;
+        fill("none");
         loop();
         return false;
     }
     path.lineSegs = [];
     return false;
+}
+
+function getStroke(x, y){
+    return 5;
 }
 
 function drawStarterLine(){
@@ -117,6 +176,7 @@ function touchEnded() {
     sendLineData();
     clear();
     selectionComplete = 0;
+    secondStage = 0;
     drawColorChoices();
     return false;
  }
@@ -130,7 +190,6 @@ function sendLineData () {
         var xhr = new XMLHttpRequest();
         xhr.open("POST", URL, true);
         xhr.setRequestHeader('Content-Type', 'application/json');
-        // path.lineSegs = path.lineSegs;
         path.strokeColor = colorPicked;
 
         xhr.onreadystatechange = function() {//Call a function when the state changes.
